c5920a24fb935cc23ca5bcd720cbff56b50d2853
Yazil Ansari <yazilansari@gmail.com>
2025-02-06 14:28:09 +0400

    Changed API URL

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
           },
           {
             protocol: 'https',
-            hostname: 'adminksa.ahmed-perfume.com',
+            hostname: 'adminksa.ahmedalmaghribi.com',
           },
           {
             protocol: 'http',
