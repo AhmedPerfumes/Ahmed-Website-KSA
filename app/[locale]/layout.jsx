1c2f4f2e029c41d4f2e4666f5fd8f9597c2033ed
Saad Edroos <saadedroossaad@gmail.com>
2025-02-24 16:21:11 +0400

    Change logo

--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -5,6 +5,7 @@ import "../../public/assets/sass/style.scss";
 import "rc-slider/assets/index.css";
 import "tippy.js/dist/tippy.css";
 import LoginFormPopup from "@/components/common/LoginFormPopup";
+import Script from "next/script";
 import ScrollTop from "@/components/common/ScrollTop";
 import Context from "@/context/Context";
 import { MenuProvider } from "@/context/MenuContext";
@@ -59,37 +60,30 @@ export default async function LocaleLayout({ children, params: { locale } }) {
 
     // Fetch translation messages
     const messages = await getMessages();
+    const GTM_ID = "GTM-M6DDMJRN";
 
     return (
         <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
-            <Head>
-                {/* Facebook Pixel Script */}
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            !function(f,b,e,v,n,t,s)
-                            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                            n.queue=[];t=b.createElement(e);t.async=!0;
-                            t.src=v;s=b.getElementsByTagName(e)[0];
-                            s.parentNode.insertBefore(t,s)}(window,document,'script',
-                            'https://connect.facebook.net/en_US/fbevents.js');
-                            fbq('init', '235034997951707'); 
-                            fbq('track', 'PageView');
-                            `,
-                    }}
-                />
-                <noscript>
-                    <img
-                        height="1"
-                        width="1"
-                        src="https://www.facebook.com/tr?id=235034997951707&ev=PageView&noscript=1"
-                        alt=""
-                    />
-                </noscript>
-            </Head>
+            <head>
+            <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+            </head>
             <body className={selectedFont.className}>
+            <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
                 <NextIntlClientProvider messages={messages}>
                     <Svgs />
                     <Context>
