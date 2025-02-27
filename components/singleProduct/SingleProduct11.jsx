8e842853df8cd30546f1bbdc6a76bef7af795f1a
Yazil Ansari <yazilansari@gmail.com>
2025-02-27 17:39:42 +0400

    Added Tabby Widget on  Product Details and Cart page

--- components/singleProduct/SingleProduct11.jsx
+++ components/singleProduct/SingleProduct11.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Slider4 from "./sliders/Slider4";
 import BreadCumb from "./BreadCumb";
 import Star from "../common/Star";
@@ -104,6 +104,47 @@ export default function SingleProduct11({ category, subcategory, product }) {
     }
   };
 
+  const tabbyPrice = (elm) => {
+    const currentUTC = new Date(); // Current UTC time
+    const currentGST = new Date(currentUTC.getTime() + (4 * 60 * 60 * 1000)); // Add 4 hours for GST
+    const current_date_time = currentGST.toISOString().slice(0, 19).replace("T", " ");
+    if(elm?.discount) {
+      if(new Date(current_date_time) >= new Date(elm.discount.start_date) && new Date(current_date_time) <= new Date(elm.discount.end_date)) {
+        return (elm.price - (elm.price / 100 * elm.discount.value)).toFixed(2);
+      } else {
+        return elm?.price;
+      }
+    } else if(elm?.sale_price) {
+      return (elm.price - (elm.price / 100 * elm.sale_price)).toFixed(2);
+    } else {
+      return elm?.price;
+    }
+  };
+
+  useEffect(() => {
+    // Load the TabbyPromo script
+    const tabbyPromoScript = document.createElement("script");
+    tabbyPromoScript.src = "https://checkout.tabby.ai/tabby-promo.js";
+    tabbyPromoScript.async = true;
+    document.body.appendChild(tabbyPromoScript);
+
+    tabbyPromoScript.onload = () => {
+      new window.TabbyPromo({
+          selector: '#TabbyPromo', // required, content of tabby Promo Snippet will be placed in element with that selector.
+          currency: 'SAR', // required, AED|SAR|KWD only supported, with no spaces or lowercase.
+          price: tabbyPrice(product), // required, price of the product. 2 decimals max for AED|SAR and 3 decimals max for KWD.
+          lang: 'en', // Optional, en|ar only supported
+          source: 'product', // Optional, snippet placement; `product` for product page and `cart` for cart page.
+          publicKey: 'pk_test_019228fd-8e52-3ecd-f813-bf11dc8e2118', // required, Public Key
+          merchantCode: 'assaaste'  // required
+      });
+    };
+
+    return () => {
+      document.body.removeChild(tabbyPromoScript);
+    };
+  }, []);
+
   return (
     <>
       {Object.keys(product).length > 0 ? <><section className="product-single container product-single__type-9">
@@ -125,6 +166,7 @@ export default function SingleProduct11({ category, subcategory, product }) {
             <div className="product-single__short-desc">
               <div dangerouslySetInnerHTML={{ __html: t.raw(cleanProductName(product.product_name)) }}></div>
             </div>
+            <div id="TabbyPromo"></div>
             <h6 style={{ color: "red" }}>{error && error}</h6>
             <form onSubmit={(e) => e.preventDefault()}>
               {product.product_qty > 0 &&
