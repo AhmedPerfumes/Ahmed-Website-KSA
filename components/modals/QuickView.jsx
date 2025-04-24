0a1b20a478155a2ea66b559bcdc6c9a0752a9234
Saad Edroos <saadedroossaad@gmail.com>
2025-04-24 10:03:34 +0400

    Added translation on newsletter

--- components/modals/QuickView.jsx
+++ components/modals/QuickView.jsx
@@ -289,8 +289,8 @@ export default function QuickView() {
                     className="btn btn-primary btn-addtocart js-open-aside"
                   >
                     {isAddedToCartProducts(quickViewItem.product_id)
-                      ? "Already Added"
-                      : "Add To Cart"}
+                      ? t("Already Added")
+                      : t("Add To Cart")}
                   </button>
                 </div>
                 ):(
