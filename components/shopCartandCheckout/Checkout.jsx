5b969ddcb8a232d0fe919ef8d84bf09310bba059
Yazil Ansari <yazilansari@gmail.com>
2025-06-12 17:21:04 +0400

    Changes Related to Coupon and FOC

--- components/shopCartandCheckout/Checkout.jsx
+++ components/shopCartandCheckout/Checkout.jsx
@@ -626,7 +626,7 @@ export default function Checkout() {
         // });
       // });
         if(new Date(current_date_time) >= new Date(elm.coupon[couponCode.toLowerCase()]?.start_date) && new Date(current_date_time) <= new Date(elm.coupon[couponCode.toLowerCase()]?.end_date) && elm.coupon[couponCode.toLowerCase()].code == couponData.code.toLowerCase()) {
-          return <td><span className="money price price-old">{ currency.symbol }{elm?.price}</span><span className="money price price-sale">{ currency.symbol }{((elm.price - (elm.price / 100 * elm.coupon[couponCode.toLowerCase()]?.value)) * elm.quantity).toFixed(2)}</span></td>;
+          return <td><span className="money price price-old">{ currency.symbol }{(elm.price * elm.quantity).toFixed(2)}</span><span className="money price price-sale">{ currency.symbol }{((elm.price - (elm.price / 100 * elm.coupon[couponCode.toLowerCase()]?.value)) * elm.quantity).toFixed(2)}</span></td>;
         } else {
           return <td>{(elm.price * elm.quantity).toFixed(2)}{ currency.symbol }</td>;
         }
