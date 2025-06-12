5585584a724218edb522292f486461cb3a1aefd3
Yazil Ansari <yazilansari@gmail.com>
2025-06-12 16:45:03 +0400

    Added Coupon Condition

--- context/Context.jsx
+++ context/Context.jsx
@@ -29,7 +29,7 @@ export default function Context({ children }) {
       } else if(product?.sale_price) {
         const sale_price = (product.price - (product.price / 100 * product.sale_price)).toFixed(2);
         return accumulator + product.quantity * sale_price;
-      } else if(product?.coupon && couponDataContext != null) {
+      } else if(product?.coupon && !Array.isArray(product.coupon) && couponDataContext != null) {
         if(new Date(current_date_time) >= new Date(product.coupon[couponDataContext?.code.toLowerCase()]?.start_date) && new Date(current_date_time) <= new Date(product.coupon[couponDataContext?.code.toLowerCase()]?.end_date) && product.coupon[couponDataContext?.code.toLowerCase()]?.code == couponDataContext?.code.toLowerCase()) {
           const coupon_price = (product.price - (product.price / 100 * product.coupon[couponDataContext?.code.toLowerCase()]?.value)).toFixed(2);
           return accumulator + product.quantity * coupon_price;
