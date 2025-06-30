02a488ccc9692a47ead0d2a3edd995872f0c5ca8
Saad Edroos <saadedroossaad@gmail.com>
2025-06-30 17:43:42 +0400

    Added xml and terms page

--- components/otherPages/Terms.jsx
+++ components/otherPages/Terms.jsx
@@ -10,7 +10,7 @@ export default function Terms() {
       </h6> */}
       
       <p className="mb-4 pb-3 fs-6">
-      Please read these Terms and Conditions (“Terms and Conditions”) carefully before using the https://www.ahmed-perfume.com website operated by Ahmed Al Maghribi Perfumes LLC (“us”, “we”, or “our”). Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service. By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service. Purchases If you wish to purchase any product or service made available through the Service (“Purchase”), you may be asked to supply certain information relevant to your Purchase including, without limitation, your … The Purchases section is for businesses that sell online (physical or digital).
+      Please read these Terms and Conditions (“Terms and Conditions”) carefully before using the https://ksa.ahmedalmaghribi.com/en website operated by Ahmed Al Maghribi Perfumes LLC (“us”, “we”, or “our”). Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service. By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service. Purchases If you wish to purchase any product or service made available through the Service (“Purchase”), you may be asked to supply certain information relevant to your Purchase including, without limitation, your … The Purchases section is for businesses that sell online (physical or digital).
       </p>
       <h6 className="mb-3"></h6>
       <p className="mb-4 pb-3 fs-6">
@@ -20,10 +20,72 @@ export default function Terms() {
       <p className="mb-4 pb-3 fs-6">
       You further acknowledge and agree that Ahmed Al Maghribi Perfumes shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services. Changes we reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is a material we will try to provide at least 30 days notice prior to any new terms taking effect. What constitutes a material change will be determined at our sole discretion. Contact Us If you have any questions about these Terms, please contact us.
       </p>
+      <p className="mb-4 pb-3 fs-6">
+      <span className="fw-bold">Tabby and Tamara orders: It cannot be cancelled or refunded</span><br/>
+      <span className="fw-bold">Orders will take 5-10 working days</span><br />
+      All credit/debit cards details and personally identifiable information will NOT be stored, sold, shared, rented or leased to any third parties. <br />
+      – Saudi Arabia is our country of domicile <br />
+      -Website  will NOT deal or provide any services or products to any of OFAC (Office of Foreign Assets Control) sanctions countries in accordance with the law of Saudi Arabia <br />
+      – We accept payments online using Visa and MasterCard credit/debit card in SAR. <br />
+      – Refunds will be done only through the Original Mode of Payment. <br />
+      – Any dispute or claim arising out of or in connection with this website shall be governed and construed in accordance with the laws of Saudi Arabia. 
+
+
+      </p>
       <h6 className="mb-3"></h6>
       <p className="mb-4 pb-3 fs-6">
       Disclaimer: Legal information is not legal advice. Read the disclaimer.
       </p>
+
+      <h2 className="section-title text-uppercase fw-bold mb-3 mt-3">
+      انشاء الحساب
+      </h2>
+      <p className="mb-4 pb-3 fs-6">
+      إذا قمت بإنشاء حساب على موقع الويب ، فأنت مسؤول عن الحفاظ على أمان حسابك وأنت مسؤول مسؤولية كاملة عن جميع الأنشطة التي تحدث ضمن الحساب وأي إجراءات أخرى يتم اتخاذها فيما يتعلق به. قد يؤدي تقديم معلومات اتصال خاطئة من أي نوع إلى إنهاء حسابك. يجب عليك إخطارنا على الفور بأي استخدامات غير مصرح بها لحسابك أو أي انتهاكات أخرى للأمن. لن نكون مسؤولين عن أي أفعال أو إغفالات من جانبك ، بما في ذلك أي أضرار من أي نوع تحدث نتيجة لهذه الأفعال أو الإغفالات. يجوز لنا تعليق حسابك أو تعطيله أو حذفه (أو أي جزء منه) إذا قررنا أنك انتهكت أي بند من أحكام هذه الاتفاقية أو أن سلوكك أو محتواك من شأنه أن يضر بسمعتنا وحسن نيتنا. 
+      </p>
+
+      <h4 className="section-title text-uppercase fw-bold mb-3 mt-3">
+      مشتريات و تجهيز الطلبات
+      </h4>
+      <p className="mb-4 pb-3 fs-6">
+      بالإضافة إلى الشروط الأخرى المنصوص عليها في الاتفاقية ، قد نختار ، وفقًا لتقديرنا الخاص ، معالجة طلبك أو إلغائه في ظروف معينة. قد يحدث هذا ، على سبيل المثال ، عندما يكون المنتج الذي ترغب في شرائه غير متوفر في المخزون أو تم تسعيره بسعر غير صحيح ، أو يكون الطلب احتياليًا ، أو قد نراه في ظروف مناسبة أخرى وفقًا لتقديرنا الخاص. نحتفظ بالحق ، وفقًا لتقديرنا الخاص ، في اتباع الإجراءات والخطوات الخاصة للتحقق من هويتك لمعالجة طلبك. مع العلم أن لدينا خيار عدم فرض رسوم عليك أو استرداد رسوم المعالجة التي لم نقم بمعالجتها أو إلغائها.
+      </p>
+      <span className="fw-bold fs-4">طلبات تابي وتامارا</span><br/>
+      <span className="fw-bold fs-4">ا يمكن إلغاؤها أو استردادها</span><br />
+      <span className="fw-bold fs-4">تستغرق الطلبات من 5 إلى 10 أيام عمل</span><br />
+
+      <h4 className="section-title text-uppercase fw-bold mb-3 mt-3">
+      سياسة التبديل و الإرجاع   
+      </h4>
+      <p className="fs-6">لا يمكن استبدال زجاجات العطور التالفة إلا بنفس العطر </p>
+      <p className="fs-6">عند طلب استبدال العطر بعطر اخر  يجب ان يكون العطر سليم ولم يتم فتحه و تحسب على المستبدل قيمة الشحن استرجاع و شحن منتج اخر</p>
+      <p className="fs-6">كاليف الشحن وبطاقة الائتمان غير قابلة للاسترداد للحزم التي لم يتم تسليمها أو عدم المطالبة بها أو إرجاعها ما لم يكن هناك خطأ من جانبنا. لن يتم رد أي تكلفة متكبدة لإعادة المنتج / المنتجات إلينا ما لم يحدث خطأ في طلبك من قبلنا.</p>
+      <p className="fs-6">ا يمكن استبدال المنتجات التي تم شراؤها من خلال موقعنا الإلكتروني  الا إذا كان المنتج الذي تلقيته تالفًا أو لم يكن المنتج الذي طلبته.</p>
+      <p className="fs-6">ن نتحمل أي مسؤولية عن فقدها أو تلفها أو تدميرها بعد تسليمها. نحن أيضًا لا نتحمل أي مسؤولية عندما تقدم عنوان تسليم غير صحيح أو عندما تفشل في جمع المنتجات من عنوان التسليم الذي حددته.</p>
+      <h4 className="section-title text-uppercase fw-bold mb-3 mt-3">
+      الرسوم والخدمات  
+      </h4>
+      <p className="fs-6">يمكن إجراء جميع المدفوعات للسلع والمنتجات على الموقع الإلكتروني عبر الإنترنت من خلال خدمات الدفع أو بطاقات الائتمان أو غيرها من وسائل الدفع المتاحة على الموقع من وقت لآخر وينتهي دورنا عند استلام مشترياتك</p>
+      <p className="fs-6">مدة تجهيز و استلام الطلب</p>
+      <p className="fs-6">يتم تأكيد و تجهيز الشحنات و استلامها من خلال شركات الشحن المعروفة في غضون 5 -12  يوم عمل </p>
+      <p className="fs-6">سياسة طلب من تابي و تمارا</p>
+      <p className="fs-6">لا يمكن الغاء او استرجاع الطلب   عند الدفع باستخدام تابي او تمارا</p>
+      <h4 className="section-title text-uppercase fw-bold mb-3 mt-3">
+      التغييرات والتعديلات 
+      </h4>
+      <p className="fs-6">
+      نحن نحتفظ بالحق في تعديل هذه الاتفاقية أو سياساتها المتعلقة بالموقع أو الخدمات في أي وقت ، وتكون سارية عند نشر نسخة محدثة من هذه الاتفاقية على الموقع. عندما نقوم بذلك ، سنقوم بمراجعة التاريخ المحدث في أسفل هذه الصفحة. استمرار استخدام الموقع بعد أي تغييرات من هذا القبيل يشكل موافقتك على هذه التغييرات
+      </p>
+      <h4 className="section-title text-uppercase fw-bold mb-3 mt-3">
+      قبول هذه الشروط  
+      </h4>
+      <p className="fs-6">أنت تقر بأنك قد قرأت هذه الاتفاقية وتوافق على جميع الشروط والأحكام الخاصة بها. باستخدام موقع الويب أو خدماته ، فإنك توافق على الالتزام بهذه الاتفاقية. إذا كنت لا توافق على الالتزام بشروط هذه الاتفاقية ، فأنت غير مصرح لك باستخدام أو الوصول إلى موقع الويب وخدماته. الاتصال بنا إذا كانت لديك أي أسئلة حول هذه الاتفاقية ، فيرجى الاتصال بنا.</p>
+
+
+
+
+
+
   
       
     </section>
