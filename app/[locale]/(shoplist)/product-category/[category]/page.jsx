143804381efdc72998beda669af8c53c4da54583
Saad Edroos <saadedroossaad@gmail.com>
2025-09-26 15:25:13 +0400

    ADded

--- app/[locale]/(shoplist)/product-category/[category]/page.jsx
+++ app/[locale]/(shoplist)/product-category/[category]/page.jsx
@@ -123,10 +123,12 @@ const ShopPage8 = async ({ params }) => {
         <QuickView />
         <Header14 />
         <Banner5 image={ data.image } mobile_image={data.mobile_image}/>
-        <main className="page-wrapper pt-0">
-          <Categories description={ data.description } subCategories={ data.productSubCategories }/>
+       <main className="page-wrapper pt-0">
+          <Categories subCategories={ data.productSubCategories }/>
           <div className="mb-4 pb-lg-3"></div>
           <Shop10 subCategories={ data.productSubCategories } products={ data.products }/>
+          <div className="mb-4 pb-lg-3"></div>
+          <CollapsibleDescription description={data.description} />
         </main>
         <div className="mb-5 pb-xl-5"></div>
         <section className="d-none d-lg-block" style={{ height: "100%" }}>
