5037a49da5a49f802a0e3a697a2932aa711c5d46
Yazil Ansari <yazilansari@gmail.com>
2025-11-18 12:02:46 +0400

    Changed Default Locale to ar

--- i18n/request.js
+++ i18n/request.js
@@ -1,17 +1,33 @@
+// import {getRequestConfig} from 'next-intl/server';
+// import {routing} from './routing';
+ 
+// export default getRequestConfig(async ({requestLocale}) => {
+//   // This typically corresponds to the `[locale]` segment
+//   let locale = await requestLocale;
+ 
+//   // Ensure that a valid locale is used
+//   if (!locale || !routing.locales.includes(locale)) {
+//     locale = routing.defaultLocale;
+//   }
+ 
+//   return {
+//     locale,
+//     messages: (await import(`../messages/${locale}.json`)).default
+//   };
+// });
+
 import {getRequestConfig} from 'next-intl/server';
 import {routing} from './routing';
- 
-export default getRequestConfig(async ({requestLocale}) => {
-  // This typically corresponds to the `[locale]` segment
-  let locale = await requestLocale;
- 
-  // Ensure that a valid locale is used
-  if (!locale || !routing.locales.includes(locale)) {
-    locale = routing.defaultLocale;
+
+export default getRequestConfig(async ({locale}) => {
+  let activeLocale = locale;
+
+  if (!routing.locales.includes(activeLocale)) {
+    activeLocale = routing.defaultLocale; // 'ar'
   }
- 
+
   return {
-    locale,
-    messages: (await import(`../messages/${locale}.json`)).default
+    locale: activeLocale,
+    messages: (await import(`../messages/${activeLocale}.json`)).default
   };
 });
