ab223571d9822a5ea5cd9e60710c39943a6e8b12
saood ghare <saoodghare0809@gmail.com>
2026-02-13 19:01:53 +0400

    commented tamara

--- components/shopCartandCheckout/Checkout.jsx
+++ components/shopCartandCheckout/Checkout.jsx
@@ -1144,13 +1144,13 @@ export default function Checkout() {
                     </label>
                   </div> */}
 
-                  <div className="form-check">
+                  {/* <div className="form-check">
                     <input className="form-check-input form-check-input_fill" type="radio" name="checkout_payment_method" id="checkout_payment_method_5" value={'tamara'} checked={selectedOption === 'tamara'} onChange={handleRadioChange} />
                     <label className="form-check-label" htmlFor="checkout_payment_method_5" style={{display: "inline-flex"}} >
                       Tamara - No interest, No fees. 
                       <TamaraWidget inlineType='4' inlineVariant='text' locale={locale}/>
                     </label>
-                  </div>
+                  </div> */}
 
                   <div className="form-check">
                     <input className="form-check-input form-check-input_fill" type="radio" name="checkout_payment_method" id="checkout_payment_method_6" value={'tabby'} checked={selectedOption === 'tabby'} onChange={handleRadioChange} />
